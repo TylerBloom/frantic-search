ffaf8ce59c329ee261ebda43ec25f334075a5737
TylerBloom <tylerbloom2222@gmail.com>
2026-04-06 13:54:56 -0400

    [updater] Reworked the updater

--- frantic-updater/src/main.rs
+++ frantic-updater/src/main.rs
@@ -1,31 +1,24 @@
-use chrono::{Datelike, Utc};
+use chrono::{Datelike, Days, Utc};
 use frantic_client::FranticClient;
 use reqwest::Client;
-use time::{Date, Month};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let fs_client = FranticClient::connect().await.unwrap();
-    // Make sure the DB is in an expected state and that the prior run was not a failure
-    // TODO: Act on the errors if any
-    fs_client.health_check().await.unwrap();
-
+    let cred_path = std::env::var("FIREBASE_CREDS")?;
+    let fs_client = FranticClient::connect_with_cred(cred_path).await.unwrap();
     let now = Utc::now();
-    let now = Date::from_ordinal_date(now.year(), now.ordinal() as u16).unwrap();
 
-    let mut date = match fs_client.fetch_latest_cr_and_metadata().await.unwrap() {
-        Some((_, prior_date, _)) => prior_date,
-        // This means the DB has no rules. To find rules, we'll start from the beginning of the
-        // year. This can still cause problems, but its good enough for now...
-        None => Date::from_calendar_date(now.year(), Month::January, 1).unwrap(),
-    };
+    // Get the date that this was last ran. There will be a logs collection.
+    let mut date = fs_client.fetch_last_update_log().await.unwrap();
 
+    // Loop until we can WOTC for today's rules.
     let client = Client::new();
     loop {
+        println!("Checking rules for {date}");
         let url = format!(
             "https://media.wizards.com/{0}/downloads/MagicCompRules {0}{1:0>2}{2:0>2}.txt",
             date.year(),
-            date.month() as u8,
+            date.month(),
             date.day(),
         );
         let resp = client.get(url).send().await.unwrap();
@@ -33,12 +26,15 @@ async fn main() -> anyhow::Result<()> {
             println!("Found CR text from {date}");
             let text = resp.text().await.unwrap();
             let text = frantic_core::normalize_cr_text(&text);
-            fs_client.insert_new_cr(text, date).await.unwrap();
+            fs_client.write(text, date).await.unwrap();
         }
 
-        date = date.next_day().unwrap();
+        date = date + Days::new(1);
         if date > now {
-            break Ok(());
+            break;
         }
     }
+
+    println!("Writing to update log");
+    fs_client.write_to_log().await
 }
