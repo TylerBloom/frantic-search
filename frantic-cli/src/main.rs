c1aa42d0c06784d307bd622a10fadae825e6e351
TylerBloom <tylerbloom2222@gmail.com>
2026-04-13 21:07:00 -0400

    [cli] Temp work around

--- frantic-cli/src/main.rs
+++ frantic-cli/src/main.rs
@@ -6,5 +6,5 @@ fn main() {
     let mut args = args();
     args.next();
     let words: Vec<_> = args.collect();
-    println!("{}", Cr::latest().search(&words));
+    // println!("{}", Cr::latest().search(&words));
 }
